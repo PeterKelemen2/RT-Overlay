19c51f256c7928a96a186ca8f923d01ac87cf0e3
PeterKelemen2 <90985214+PeterKelemen2@users.noreply.github.com>
2022-10-15 01:43:49 +0300

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,4 @@
+cd "C:\Users\%USERNAME%\Downloads\Custom Overlay"
+copy *.ovl "C:\Program Files (x86)\RivaTuner Statistics Server\Plugins\Client\Overlays\"
+
+PAUSE
---
+++ Open Folder.bat
@@ -0,0 +1,2 @@
+
+explorer "C:\Program Files (x86)\RivaTuner Statistics Server\Plugins\Client\Overlays\"
